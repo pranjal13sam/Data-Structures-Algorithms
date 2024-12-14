7f819de58e74357ff6c32f0f3b38f9d56b7aaa1f
Pranjal <pandeypranjal264@gmail.com>
2024-12-14 22:14:43 +0530

    updated

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -1,141 +1,184 @@
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//1st pattern:
-/*   
-    ****
-    ****
-    ****   
-*/
+// 1st pattern:
+/*
+ ****
+ ****
+ ****
+ */
 
-void pattern1(int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n;j++){
-            cout<<"*";
+void pattern1(int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
+// 2nd pattern:
+/*
+ *
+ **
+ ***
+ */
 
-//2nd pattern:
-/*   
-    *
-    **
-    ***  
-*/
-
-void pattern2(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<"*";
+void pattern2(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-//3rd pattern:
-/*   
+// 3rd pattern:
+/*
     1
     12
-    123  
+    123
 */
 
-void pattern3(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<j,"\t";
+void pattern3(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j, "\t";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-//4th pattern:
-/*   
+// 4th pattern:
+/*
     1
     22
-    333  
+    333
 */
 
-void pattern4(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<i;
+void pattern4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-//5th pattern:
-/*   
-    ****
-    ***
-    **
-    *
-*/
+// 5th pattern:
+/*
+ ****
+ ***
+ **
+ *
+ */
 
-//formula for reverse patterns: (n-i+1)
-void pattern5(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=n-i+1;j>0;j--){
-            cout<<"*";
+// formula for reverse patterns: (n-i+1)
+void pattern5(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - i + 1; j > 0; j--)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-//6th pattern:
-/*   
+// 6th pattern:
+/*
     12345
     1234
     12
     1
 */
 
-//formula for reverse patterns: (n-i+1)
-void pattern6(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=n;j>n-(n-i);j--){
-            cout<<"*";
+// formula for reverse patterns: (n-i+1)
+void pattern6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j > n - (n - i); j--)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-//7th pattern:
-/*   
-    *
-   *** 
-  *****
-*/
-
+// 7th pattern:
+/*
+ *
+ ***
+ *****
+ */
 
 void pattern7(int n)
 {
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-    for(int j=0;j<=n-i-1;j++){
-        cout<<" ";
-    }
-    for(int j=0;j<i*2+1;j++){
-        cout<<"*";
-    }
-     for(int j=0;j<=n-i-1;j++){
-        cout<<" ";
-    }
-    cout<<endl;
+        for (int j = 0; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i * 2 + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
     }
 }
 
+// 7th pattern:
+/*
+ *****
+ ***
+ *
+ */
 
+void pattern8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= n-(n-i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2*n-(2*i+1); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j <= n-(n-i); j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 
-    
 int main()
 {
     int n;
-    cout<<"Enter the value of n: "<<endl;
-    cin>>n;
-    pattern7(n);
+    cout << "Enter the value of n: " << endl;
+    cin >> n;
+    pattern8(n);
     return 0;
 }
