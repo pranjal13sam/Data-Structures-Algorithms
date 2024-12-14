85eccdd447ca726ac7d71b8d2a38f4f1461cc028
Pranjal <pandeypranjal264@gmail.com>
2024-12-14 21:17:22 +0530

    updated_code

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 
-//1st patern:
+//1st pattern:
 /*   
     ****
     ****
@@ -19,13 +19,123 @@ void pattern1(int n){
 }
 
 
+//2nd pattern:
+/*   
+    *
+    **
+    ***  
+*/
+
+void pattern2(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+//3rd pattern:
+/*   
+    1
+    12
+    123  
+*/
+
+void pattern3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j,"\t";
+        }
+        cout<<endl;
+    }
+}
+
+//4th pattern:
+/*   
+    1
+    22
+    333  
+*/
+
+void pattern4(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+
+//5th pattern:
+/*   
+    ****
+    ***
+    **
+    *
+*/
+
+//formula for reverse patterns: (n-i+1)
+void pattern5(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=n-i+1;j>0;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+//6th pattern:
+/*   
+    12345
+    1234
+    12
+    1
+*/
+
+//formula for reverse patterns: (n-i+1)
+void pattern6(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>n-(n-i);j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+//7th pattern:
+/*   
+    *
+   *** 
+  *****
+*/
+
+
+void pattern7(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+    for(int j=0;j<=n-i-1;j++){
+        cout<<" ";
+    }
+    for(int j=0;j<i*2+1;j++){
+        cout<<"*";
+    }
+     for(int j=0;j<=n-i-1;j++){
+        cout<<" ";
+    }
+    cout<<endl;
+    }
+}
+
+
 
     
 int main()
 {
-    int n=3;
+    int n;
     cout<<"Enter the value of n: "<<endl;
     cin>>n;
-    pattern1(n);
+    pattern7(n);
     return 0;
 }
